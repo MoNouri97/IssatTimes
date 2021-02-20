3c8a0ac5f2c005e2871c5449286e2d0dd918080c
Mohamed Nouri <mohamed.nouri.1997@gmail.com>
2021-02-20 20:17:16 +0100

    statusBar follows theme

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { NavigationContainer } from '@react-navigation/native';
 import React, { useState } from 'react';
 import { StatusBar, StyleSheet, View } from 'react-native';
 import {
@@ -8,7 +7,6 @@ import {
 	Lato_900Black,
 } from '@expo-google-fonts/lato';
 
-import Tabs from './app/screens/Tabs';
 import { SubjectsContext } from './app/context/Subjects/SubjectsContext';
 import { useSubjectsState } from './app/context/Subjects/useSubjectsState';
 import AppScreen from './app/components/AppScreen';
@@ -18,18 +16,13 @@ import { GroupProvider } from './app/context/Group/GroupContext';
 import { TodosProvider } from './app/context/Todos/TodosContext';
 import SelectGroup from './app/screens/SelectGroup';
 import { AppLoading } from 'expo';
-import { groupInfo, Subject } from './app/types';
+import { Subject } from './app/types';
 import { loadStateFromStorage } from './app/utils/ManageAsyncStorage';
-import { keys, MyTheme } from './app/config/vars';
+import { keys } from './app/config/vars';
 import color from './app/config/color';
-import {
-	createStackNavigator,
-	TransitionPresets,
-} from '@react-navigation/stack';
-import settingsScreen from './app/screens/settingsScreen';
-import MainScreen from './app/screens/MainScreen';
 import { ThemeProvider } from './app/context/Theme/ThemeContext';
 import MainNavContainer from './app/components/MainNavContainer';
+import AppStatusBar from './app/components/AppStatusBar';
 
 export default function App() {
 	const [isReady, setIsReady] = useState(false);
@@ -62,8 +55,8 @@ export default function App() {
 	// return <TestScreen />;
 	return (
 		<AppScreen style={styles.container}>
-			<StatusBar backgroundColor={color.bg} barStyle='light-content' />
 			<ThemeProvider>
+				<AppStatusBar />
 				<GroupProvider>
 					<SubjectsContext.Provider value={subjectsValue}>
 						<TodosProvider>
